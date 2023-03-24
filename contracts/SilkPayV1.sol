479309e6b06817a94ee21b31288d443f01659497
yuchuan.song <yuchuan.song@debank.com>
2023-03-24 22:27:24 +0800

    add some functions about arbitration

--- contracts/SilkPayV1.sol
+++ contracts/SilkPayV1.sol
@@ -23,14 +23,16 @@ contract SilkPayV1 is Pausable {
 
     uint8 constant SENDER_WINS = 1;
     uint8 constant RECIPIENT_WINS = 2;
-    uint16 constant MIN_LOCK_TIME = 7200;
+    uint16 constant public MIN_LOCK_TIME = 7200;
+    bytes32 constant ZEROBYTES32= bytes32(0x00);
 
     address public owner;
     uint256 public gracePeriod;
     Arbitrator public arbitrator;
 
     PaymentUtils.Payment[] public payments;
-    mapping(uint256 => uint256) public PaymentId2DisputeId;
+    mapping(uint256 => uint256) public disputeIDtoPaymentId;
+    mapping(uint256 => uint256) public PaymentIdtoDisputeId;
 
     event PaymentCreated(
         uint256 indexed PaymentID, 
@@ -45,6 +47,7 @@ contract SilkPayV1 is Pausable {
     event RecipientSpecify(uint256 indexed PaymentID, address indexed recipient);
     event PayFinished(uint256 indexed PaymentID, address indexed sender, address indexed recipient, uint256 amount);
     event ReFund(uint256 indexed PaymentID, address indexed sender, uint256 amount);
+    event Evidence(address indexed arbitrator, uint256 indexed PaymentID, address indexed submitter, string _evidence);
 
     constructor (
         Arbitrator _arbitrator,
@@ -77,10 +80,10 @@ contract SilkPayV1 is Pausable {
     ) public payable whenNotPaused returns (uint256 PaymentID) {
         if (targeted) {
             require(recipient != address(0));
-            require(merkleTreeRoot == bytes32(0x00));
+            require(merkleTreeRoot == ZEROBYTES32);
         } else {
             require(recipient == address(0));
-            require(merkleTreeRoot != bytes32(0x00));
+            require(merkleTreeRoot != ZEROBYTES32);
         }
         require(lockTime >= MIN_LOCK_TIME, "lock time should greater or equal to 7200 seconds");
         require(msg.value > 0, "amount should not be zero");
@@ -101,17 +104,21 @@ contract SilkPayV1 is Pausable {
         return payments.length - 1;
     }
 
+    function _verifyRecipient(PaymentUtils.Payment storage payment, bytes32[] memory proof, address recipient) internal view returns (bool) {
+        require(proof.length > 0, "proof should not be empty");
+        require(payment.merkleTreeRoot != ZEROBYTES32);
+
+        bytes32 leafHash = keccak256(abi.encodePacked(recipient));
+        return MerkleProof.verify(proof, payment.merkleTreeRoot, leafHash);
+    }
+
     /**
      * @dev Verify whether the recipient address is in the recipient merkletree of a certain payment
      * 
      */
     function verifyRecipient(uint256 PaymentID, bytes32[] memory proof, address recipient) public view returns (bool) {
-        require(proof.length > 0, "proof should not be empty");
         PaymentUtils.Payment storage payment = payments[PaymentID];
-        require(payment.merkleTreeRoot != bytes32(0x00));
-
-        bytes32 leafHash = keccak256(abi.encodePacked(recipient));
-        return MerkleProof.verify(proof, payment.merkleTreeRoot, leafHash);
+        return _verifyRecipient(payment, proof, recipient);
     }
 
     function specifyRecipient(uint256 PaymentID, bytes32[] memory proof, address recipient) public {
@@ -176,12 +183,68 @@ contract SilkPayV1 is Pausable {
         // emit DisputeCreated();
     }
 
-    // function rule(uint256 _disputeId, uint256 _ruling) external override {
-    //     // 法官才能裁决
-    //     // 需要在裁决阶段才能裁决
-    //     // 仲裁费用需要结算
+    function onlyTargeted(PaymentUtils.Payment storage payment) internal view {
+        require(payment.targeted, "only targeted");
+    }
 
-    // }
+    function onlySender(PaymentUtils.Payment storage payment) internal view {
+        require(msg.sender == payment.sender, "The caller must be the sender");
+    }
+
+    function onlyRecipient(PaymentUtils.Payment storage payment) internal view {
+        require(msg.sender == payment.recipient, "The caller must be the recipient");
+    }
+
+    function onlyParticipant(PaymentUtils.Payment storage payment, bytes32[] memory proof) internal view {
+        require(!payment.targeted && payment.merkleTreeRoot != ZEROBYTES32, "only not targeted");
+        require(_verifyRecipient(payment, proof, msg.sender), "The caller should be participant");
+    }
+
+    function onlyDisputing(PaymentUtils.Payment storage payment) internal view {
+        require(payment.status == PaymentUtils.PaymentStatus.Appealing, "only in a dispute");
+    }
+
+    function submitEvidenceBySender(uint256 PaymentID, string calldata _evidence) external {
+        PaymentUtils.Payment storage payment = payments[PaymentID];
+        onlyDisputing(payment);
+        onlySender(payment);
+        _submitEvidence(PaymentID, _evidence);
+    }
+
+    function submitEvidenceByRecipient(uint256 PaymentID, string calldata _evidence) external {
+        PaymentUtils.Payment storage payment = payments[PaymentID];
+        onlyDisputing(payment);
+        onlyTargeted(payment);
+        onlyRecipient(payment);
+        _submitEvidence(PaymentID, _evidence);
+    }
+
+    function submitEvidenceByParticipant(uint256 PaymentID, string calldata _evidence, bytes32[] memory proof) external {
+        PaymentUtils.Payment storage payment = payments[PaymentID];
+        onlyDisputing(payment);
+        onlyParticipant(payment, proof);
+        _submitEvidence(PaymentID, _evidence);
+    }
+
+    /** @dev Submit a reference to evidence. EVENT.
+     *  @param PaymentID The index of a payment.
+     *  @param _evidence A link to an evidence using its URI.
+     */
+    function _submitEvidence(uint256 PaymentID, string calldata _evidence) internal {
+        // uint256 DisputeID = PaymentId2DisputeId[PaymentID];
+
+        emit Evidence(address(arbitrator), PaymentID, msg.sender, _evidence);
+    }
+
+    function rule(uint256 _disputeId, uint256 _ruling) external {
+        require(msg.sender == address(arbitrator), "The caller must be the arbitrator.");
+        uint256 PaymentId = disputeIDtoPaymentId[_disputeId];
+        PaymentUtils.Payment storage payment = payments[PaymentId];
+
+        // 需要在裁决阶段才能裁决 TODO
+        // 仲裁费用需要结算 TODO
+        settleFee(payment, _ruling);
+    }
 
     function settleFee(PaymentUtils.Payment storage payment, uint256 _ruling) internal {
         require(_ruling == SENDER_WINS || _ruling == RECIPIENT_WINS);
@@ -189,6 +252,10 @@ contract SilkPayV1 is Pausable {
             payable(payment.sender).transfer(payment.amount);
         } else if (_ruling == RECIPIENT_WINS) {
             payable(payment.recipient).transfer(payment.amount);
+        } else {
+            uint256 split_amount = payment.amount / 2;
+            payable(payment.recipient).transfer(split_amount);
+            payable(payment.sender).transfer(split_amount);
         }
 
         payment.amount = 0;
